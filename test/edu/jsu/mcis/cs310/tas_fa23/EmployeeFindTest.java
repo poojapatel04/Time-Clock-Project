bc558b3ad84a662825e0d1868ab6ba4a6d066800
Pooja patel <poojapatel040201@gmail.com>
2023-10-30 11:24:15 -0500

    Employee test added

--- test/edu/jsu/mcis/cs310/tas_fa23/EmployeeFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa23/EmployeeFindTest.java
@@ -80,5 +80,21 @@ public class EmployeeFindTest {
         assertEquals("ID #93: Leist, Rodney J (#C1E4758D), Type: Temporary / Part-Time, Department: Warehouse, Active: 10/09/2015", e4.toString());
 
     }
+    @Test
+    public void testFindEmployee5() {
+        
+        EmployeeDAO employeeDAO = daoFactory.getEmployeeDAO();
+        BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
+
+        /* Retrieve Employee from Database (by badge) */
+
+        Badge b = badgeDAO.find("DFD9BB5C");
+        Employee e5 = employeeDAO.find(b);
+
+        /* Compare to Expected Values */
+        
+        assertEquals("ID #116: Gallegos, phillip M (#DFD9BB5C), Type: Temporary / Part-Time, Department: Warehouse, Active: 09/18/2015", e5.toString());
+    }
+
     
 }
